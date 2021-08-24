80d025f8a58c58fce61eb4633c50d5a3658aef82
careydevelopment <mrbrianmcarey@gmail.com>
2021-08-24 07:03:43 -0400

    added support for recaptcha

--- src/main/java/us/careydevelopment/ecosystem/jwt/model/JwtRequest.java
+++ src/main/java/us/careydevelopment/ecosystem/jwt/model/JwtRequest.java
@@ -2,11 +2,16 @@ package us.careydevelopment.ecosystem.jwt.model;
 
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class JwtRequest {
 	
     private String username;
     private String password;	
     private Boolean setCookie = false;
+    
+    @JsonProperty("g-recaptcha-response")
+    private String recaptchaResponse;
 	
     public JwtRequest() { }
 
@@ -37,6 +42,14 @@ public class JwtRequest {
 
     public void setSetCookie(Boolean setCookie) {
         this.setCookie = setCookie;
+    }
+    
+    public String getRecaptchaResponse() {
+        return recaptchaResponse;
+    }
+
+    public void setRecaptchaResponse(String recaptchaResponse) {
+        this.recaptchaResponse = recaptchaResponse;
     }
 
     public String toString() {
