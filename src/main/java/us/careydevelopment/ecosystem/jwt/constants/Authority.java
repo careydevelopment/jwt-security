92a555c973f5f62994eaf8f147a4db50fa98356f
careydevelopment <mrbrianmcarey@gmail.com>
2021-09-06 18:04:15 -0400

    added new authority

--- src/main/java/us/careydevelopment/ecosystem/jwt/constants/Authority.java
+++ src/main/java/us/careydevelopment/ecosystem/jwt/constants/Authority.java
@@ -4,5 +4,6 @@ public class Authority {
 
     public static final String CRM_USER = "CAREYDEVELOPMENT_CRM_USER";
     public static final String BASIC_ECOSYSTEM_USER = "CAREYDEVELOPMENT_ECOSYSTEM_BASIC";
+    public static final String ADMIN_ECOSYSTEM_USER = "CAREYDEVELOPMENT_ECOSYSTEM_ADMIN";
     
 }
